28adf45a3fd978b273cff33a106c99138d32cfbf
bonbongiraffe <francescowai@yahoo.com>
2023-06-13 11:26:54 -0400

    first attempt

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function password(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input onChange={password} type="password"/>
+        </div>
     )
 }
 
